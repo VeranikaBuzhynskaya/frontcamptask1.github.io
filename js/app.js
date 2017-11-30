dc2712827b5b359c318978347be971f08ecce2ea
VeranikaBuzhynskaya <fruittelllla@gmail.com>
2017-11-30 23:27:45 +0300

    Changed action of dropdown

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
-let news = document.getElementById('news');
+const news = document.getElementById('news');
 const defaultSource = 'bbc-news';
+const dropDownSources = document.getElementById('dropdown-content');
 
 const source = new Sources();
 source.showSources();
@@ -7,14 +8,25 @@ source.showSources();
 const article = new Articles();
 article.showArticles(defaultSource);
 
-document.getElementById('navbar').addEventListener("click", function(e){
+function handleMenu() {
+    dropDownSources.classList.toggle("show");
+}
+
+dropDownSources.addEventListener("click", e => {
     if(e.target.id){
         while (news.lastChild) {
             news.removeChild(news.lastChild);
         }
         const article = new Articles();
         article.showArticles(e.target.id);
-    } else{
-        document.getElementById('footer').innerHTML = `Made by ${e.target.innerText}`;
+    } else if(e.target.nodeName === "LABEL"){
+        document.getElementById('header').querySelector('h1').innerHTML = `${e.target.textContent}`;
+        dropDownSources.classList.remove('show');
+    }
+});
+
+document.addEventListener("click", e => {
+    if (!event.target.matches('.dropbtn')) {
+        dropDownSources.classList.remove('show');
     }
 });
