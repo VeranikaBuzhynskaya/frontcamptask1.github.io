86074ea1b97d7ddb13bd69fd912a7615e78d7cdc
VeranikaBuzhynskaya <fruittelllla@gmail.com>
2018-01-10 13:35:17 +0300

    Changed mode of button menu, when load button are show

--- js/load-button.js
+++ js/load-button.js
@@ -2,9 +2,11 @@ import '../styles/style.css';
 
 document.addEventListener('DOMContentLoaded', e => {
     const loadButton = document.getElementsByClassName('load-button')[0];
+    const menuButton = document.getElementById('navbar');
     loadButton.addEventListener('click', e => import (/* webpackChunkName: "load" */ './app.js')
     .then(module => {
         loadButton.remove();
+        menuButton.style.opacity = 1;
     }));
 })
 
